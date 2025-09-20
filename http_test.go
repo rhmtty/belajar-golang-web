b3117b69af785dba4ba42a68ec4c9aab57b4dcd3
rhmtty <rohmat.tri@gmf-aeroasia.co.id>
2025-09-20 11:24:09 +0700

    belajar golang web

---
+++ http_test.go
@@ -0,0 +1,25 @@
+package belajargolangweb
+
+import (
+	"fmt"
+	"io"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+)
+
+func HelloHandler(writer http.ResponseWriter, req *http.Request) {
+	fmt.Fprint(writer, "Hello World")
+}
+
+func TestHttp(t *testing.T) {
+	request := httptest.NewRequest(http.MethodGet, "http://localhost:8000/hello", nil)
+	recorder := httptest.NewRecorder()
+
+	HelloHandler(recorder, request)
+
+	response := recorder.Result()
+	body, _ := io.ReadAll(response.Body)
+	bodyString := string(body)
+	fmt.Println(bodyString)
+}
